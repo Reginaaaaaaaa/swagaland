67493bdbe791eca2e99d8bef21c1d360be93992c
pomoichka <89976441+Reginaaaaaaaa@users.noreply.github.com>
2026-07-12 13:20:15 +0300

    Create supabase-config.js

---
+++ supabase-config.js
@@ -0,0 +1,7 @@
+const SUPABASE_URL = "https://srswkyitboaspohxddkw.supabase.co";
+const SUPABASE_PUBLISHABLE_KEY = "sb_publishable_DkJrg5Ea22GTaI4eaueU0w_fKPhl-mq";
+
+const supabaseClient = window.supabase.createClient(
+  SUPABASE_URL,
+  SUPABASE_PUBLISHABLE_KEY
+);
